b9a4558dcba853832507fc6de33cde5174dc893b
Andrew Porrazzo <porrazzoac@vcu.edu>
2019-10-10 19:00:12 -0400

    Add project files.

---
+++ 10_10_2019/10_10_2019.cpp
@@ -0,0 +1,10 @@
+// 10_10_2019.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
